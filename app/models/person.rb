1109384a772a5fbab1fbd37ae5b0eb57cb7af273
tegansims <github email address>
2019-09-08 17:32:16 +0100

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,7 +3,7 @@ class Person < ActiveRecord::Base
   accepts_nested_attributes_for :addresses
 
   def address_attributes=(address)
-    self.address = Adress.find_or_create_by(street_address_1: address[:street_address_1])
+    self.address = Address.find_or_create_by(street_address_1: address[:street_address_1])
     self.address.update(address)
   end
 end
